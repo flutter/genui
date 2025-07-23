84101069c55c00f4bf5d34d6b4266080e808fae4
Polina Cherkasova <polina.c@live.com>
2025-07-23 13:33:25 -0700

    invitation_draft

--- pkgs/example/lib/sdk/agent/agent.dart
+++ pkgs/example/lib/sdk/agent/agent.dart
@@ -1,8 +1,15 @@
+import 'package:example/sdk/agent/fake_output.dart';
 import 'package:example/sdk/agent/input.dart';
+import 'package:example/sdk/catalog/invitation.dart';
 import 'package:flutter/widgets.dart';
 
 class GenUiAgent {
-  Widget request(Input input) {
-    return Text('Request processed: $input');
+  Future<Widget> request(Input input) async {
+    switch (input.runtimeType) {
+      case InvitationInput _:
+        return Invitation(data: invitationData);
+      default:
+        throw Exception('Unsupported input type: ${input.runtimeType}');
+    }
   }
 }
--- pkgs/example/lib/sdk/agent/input.dart
+++ pkgs/example/lib/sdk/agent/input.dart
@@ -1,3 +1,11 @@
-class Input {
-  Input.x();
+class ImageCatalog {}
+
+sealed class Input {
+  final ImageCatalog imageCatalog;
+
+  const Input._(this.imageCatalog);
+}
+
+class InvitationInput extends Input {
+  InvitationInput(super.imageCatalog) : super._();
 }
