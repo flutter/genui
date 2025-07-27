9dbcf939d22b1d76772fc3e0e73df8e281180113
Jacob Simionato <jsimionato@google.com>
2025-07-27 10:11:53 +1000

    Add text widget

--- pkgs/genui_client/lib/src/core_catalog.dart
+++ pkgs/genui_client/lib/src/core_catalog.dart
@@ -1,8 +1,10 @@
 import 'catalog.dart';
 import 'widgets/elevated_button.dart';
 import 'widgets/column.dart';
+import 'widgets/text.dart';
 
 final coreCatalog = Catalog([
   elevatedButtonCatalogItem,
   columnCatalogItem,
+  text,
 ]);
