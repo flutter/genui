b5ecdce4d606f8f3e2197bcc93241fdf8901bf92
Jacob Simionato <jsimionato@google.com>
2025-07-27 10:11:58 +0930

    Add Radio and Checbox widgets

--- pkgs/genui_client/lib/src/core_catalog.dart
+++ pkgs/genui_client/lib/src/core_catalog.dart
@@ -2,9 +2,13 @@ import 'catalog.dart';
 import 'widgets/elevated_button.dart';
 import 'widgets/column.dart';
 import 'widgets/text.dart';
+import 'widgets/checkbox_group.dart';
+import 'widgets/radio_group.dart';
 
 final coreCatalog = Catalog([
   elevatedButtonCatalogItem,
   columnCatalogItem,
   text,
+  checkboxGroup,
+  radioGroup,
 ]);
