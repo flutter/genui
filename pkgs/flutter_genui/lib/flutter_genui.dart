f231d1d368059dccb9dc8e6cb0254bbd77c6b215
Polina Cherkasova <polina.c@live.com>
2025-07-29 16:11:19 -0700

    -

--- pkgs/flutter_genui/lib/flutter_genui.dart
+++ pkgs/flutter_genui/lib/flutter_genui.dart
@@ -2,4 +2,4 @@ export 'agent/agent.dart';
 export 'model/controller.dart';
 export 'model/image_catalog.dart';
 export 'model/input.dart';
-export '_ai_client/ai_client.dart';
+export 'ai_client/ai_client.dart';
