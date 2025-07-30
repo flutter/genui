c885ee56743abe17a7a516850c8677764ecb6551
Jacob Simionato <jsimionato@google.com>
2025-07-30 12:34:19 +0930

    Move conversation management logic into widget_tree_llm_adapter

--- examples/generic_chat/lib/src/core_catalog.dart
+++ examples/generic_chat/lib/src/core_catalog.dart
@@ -1,9 +1,9 @@
 import 'catalog.dart';
-import 'widgets/elevated_button.dart';
-import 'widgets/column.dart';
-import 'widgets/text.dart';
 import 'widgets/checkbox_group.dart';
+import 'widgets/column.dart';
+import 'widgets/elevated_button.dart';
 import 'widgets/radio_group.dart';
+import 'widgets/text.dart';
 import 'widgets/text_field.dart';
 
 final coreCatalog = Catalog([
