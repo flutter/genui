c757a4e9e31066134ee79a1d048c65a85aa97a43
Greg Spencer <gspencer@google.com>
2025-07-30 19:34:13 -0700

    Adding test script.

---
+++ ci/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+package_path=$1
+
+cd "$package_path"
+echo ""
+echo "================================================="
+echo "Testing package at: $package_path"
+echo "================================================="
+echo "--- Updating submodules ---"
+git submodule update
+echo "--- Installing dependencies ---"
+dart pub get
+echo "--- Checking formatting ---"
+dart format --output=none --set-exit-if-changed .
+echo "--- Analyzing code ---"
+flutter analyze --fatal-infos
+if [[ -d "test" ]]; then
+  echo "--- Running tests ---"
+  flutter test --test-randomize-ordering-seed=random
+else
+  echo "No test directory in $package_path"
+fi
+l
