924e4fa32c331366a8a2257c7a88f79ba9e2451e
Polina Cherkasova <polina.c@live.com>
2025-08-01 11:09:26 -0700

    Update refresh_packages.sh

--- tool/refresh_packages.sh
+++ tool/refresh_packages.sh
@@ -28,11 +28,11 @@ DART_PACKAGES=(
 )
 
 for pkg in "${FLUTTER_PACKAGES[@]}"; do
-    echo "--- Running flutter pub upgrade in $pkg ---"
+    echo "--- Refreshing packages in $pkg ---"
     (cd "$pkg" && flutter pub upgrade)
 done
 
 for pkg in "${DART_PACKAGES[@]}"; do
-    echo "--- Running dart pub upgrade in $pkg ---"
+    echo "--- Refreshing packages in $pkg ---"
     (cd "$pkg" && dart pub upgrade)
 done
