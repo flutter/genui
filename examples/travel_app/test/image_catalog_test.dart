04514d67aa450d0ec5e326c9189ceffe9c157ba9
Polina Cherkasova <polina.c@live.com>
2025-08-05 15:35:51 -0700

    -

--- examples/travel_app/test/image_catalog_test.dart
+++ examples/travel_app/test/image_catalog_test.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:travel_app/src/catalog.dart';
+import 'package:travel_app/src/asset_images.dart';
 
 void main() {
   test(
