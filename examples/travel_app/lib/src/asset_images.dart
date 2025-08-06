b5c49c1bc491c67412da62de2db11abb5ea5569a
Jacob Simionato <jsimionato@google.com>
2025-08-06 11:58:57 +0930

    Fix loading of images.json in the web version. The '.' prefix seemed to break deployment.

--- examples/travel_app/lib/src/asset_images.dart
+++ examples/travel_app/lib/src/asset_images.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 @visibleForTesting
 const assetImageCatalogPath = 'assets/travel_images';
 @visibleForTesting
-const assetImageCatalogJsonFile = '$assetImageCatalogPath/.images.json';
+const assetImageCatalogJsonFile = '$assetImageCatalogPath/images.json';
 
 Future<String> assetImageCatalogJson() async {
   var result = await rootBundle.loadString(assetImageCatalogJsonFile);
