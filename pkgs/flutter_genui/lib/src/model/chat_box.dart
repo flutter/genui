27d6f4c69afddc2f5bc45ef0571207cb95b9ee41
Polina Cherkasova <polina.c@live.com>
2025-08-06 15:40:23 -0700

    -

--- pkgs/flutter_genui/lib/src/model/chat_box.dart
+++ pkgs/flutter_genui/lib/src/model/chat_box.dart
@@ -2,19 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 typedef ChatBoxCallback = void Function(String input);
 
+typedef ChatBoxBuilder = Widget Function(ChatBoxController controller);
+
+Widget defaultChatBoxBuilder(ChatBoxController controller) =>
+    ChatBox(controller);
+
+class ChatBoxController {
+  ChatBoxController({required this.onInput});
+
+  /// The chat box will stop taking input after [stopped] is completed.
+  final stopped = Completer<void>();
+
+  /// Is invoked when the user submits input.
+  ///
+  /// User can submit input multiple times, so this callback
+  /// should be able to handle multiple invocations.
+  final void Function(String input) onInput;
+
+  /// If true, the user will see a progress indicator.
+  final ValueNotifier<bool> isProcessing = ValueNotifier<bool>(true);
+
+  void dispose() {
+    stopped.complete();
+    isProcessing.dispose();
+  }
+}
+
 class ChatBox extends StatefulWidget {
   ChatBox(
-    this.onInput, {
+    this.controller, {
     super.key,
     this.borderRadius = 25.0,
     this.hintText = 'Ask me anything',
   });
 
-  final ChatBoxCallback onInput;
+  final ChatBoxController controller;
   final double borderRadius;
   final String hintText;
 
@@ -25,22 +53,27 @@ class ChatBox extends StatefulWidget {
 class _ChatBoxState extends State<ChatBox> {
   final _controller = TextEditingController();
   final _focusNode = FocusNode();
-  bool _isSubmitted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.requestFocus();
+  }
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       controller: _controller,
       focusNode: _focusNode,
-      enabled: !_isSubmitted,
       decoration: InputDecoration(
         hintText: widget.hintText,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius)),
         ),
-        suffixIcon: _isSubmitted
-            ? null
-            : IconButton(icon: const Icon(Icons.send), onPressed: _submit),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send),
+          onPressed: _submit,
+        ),
       ),
       maxLines: null, // Allows for multi-line input
       keyboardType: TextInputType.multiline,
@@ -52,13 +85,11 @@ class _ChatBoxState extends State<ChatBox> {
   void _submit() {
     final inputText = _controller.text.trim();
     _focusNode.unfocus();
-    setState(() => _isSubmitted = true);
-    widget.onInput(inputText);
+    widget.controller.onInput(inputText);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
     _focusNode.dispose();
     super.dispose();
   }
