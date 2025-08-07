ab65325157e5d20bb2d98df4fdb9495c070d3483
Polina Cherkasova <polina.c@live.com>
2025-08-07 07:38:46 -0700

    Update main.dart

--- examples/minimal_genui/lib/main.dart
+++ examples/minimal_genui/lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: _genUiManager.widget(),
+        child: Column(children: [Expanded(child: _genUiManager.widget())]),
       ),
     );
   }
