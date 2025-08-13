754ee87311222a1d01308a73bb17dfd5ef0fddf1
Polina Cherkasova <polina.c@live.com>
2025-08-13 10:13:23 -0700

    Update flutter_genui.dart

--- pkgs/flutter_genui/lib/flutter_genui.dart
+++ pkgs/flutter_genui/lib/flutter_genui.dart
@@ -14,6 +14,7 @@ export 'src/catalog/core_widgets/text_field.dart';
 export 'src/core/core_catalog.dart';
 export 'src/core/genui_manager.dart';
 export 'src/core/ui_event_manager.dart';
+export 'src/facade/firebase_ai.dart';
 export 'src/model/catalog.dart';
 export 'src/model/catalog_item.dart';
 export 'src/model/chat_box.dart';
