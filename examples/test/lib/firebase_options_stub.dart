1a51b7d32db9d303850ed5ec458524f7839098b3
Polina Cherkasova <polina.c@live.com>
2025-08-14 11:34:25 -0700

    Update firebase_options_stub.dart

--- examples/test/lib/firebase_options_stub.dart
+++ examples/test/lib/firebase_options_stub.dart
@@ -3,7 +3,8 @@ import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     throw UnimplementedError(
-      'Regenerate this file by running `flutterfire configure`. ',
+      'Regenerate `firebase_options.dart`. '
+      'See https://github.com/flutter/genui/blob/main/doc/USAGE.md#configure-firebase',
     );
   }
 }
