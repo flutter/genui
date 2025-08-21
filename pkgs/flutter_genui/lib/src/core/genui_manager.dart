01fccf8f1192ae4c6b16ff5be45ce132257ac84a
Polina Cherkasova <polina.c@live.com>
2025-08-21 14:29:14 -0700

    -

--- pkgs/flutter_genui/lib/src/core/genui_manager.dart
+++ pkgs/flutter_genui/lib/src/core/genui_manager.dart
@@ -3,11 +3,24 @@
 // found in the LICENSE file.
 
 import '../model/catalog.dart';
+import '../model/tools.dart';
 import 'surface_manager.dart';
+import 'ui_tools.dart';
 
 class GenuiManager {
   SurfaceManager surfaceManager;
 
   GenuiManager({Catalog? catalog})
     : surfaceManager = SurfaceManager(catalog: catalog);
+
+  /// Returns a list of [AiTool]s that can be used to manipulate the UI.
+  ///
+  /// These tools should be provided to the [AiClient] to allow the AI to
+  /// generate and modify the UI.
+  List<AiTool> getTools() {
+    return [
+      AddOrUpdateSurfaceTool(surfaceManager),
+      DeleteSurfaceTool(surfaceManager),
+    ];
+  }
 }
