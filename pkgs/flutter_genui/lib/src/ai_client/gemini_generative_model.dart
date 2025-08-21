cc15680c105938e18b1cf5925ed75c34cca81b30
Greg Spencer <gspencer@google.com>
2025-08-21 09:52:13 -0700

    Fix copyright

--- pkgs/flutter_genui/lib/src/ai_client/gemini_generative_model.dart
+++ pkgs/flutter_genui/lib/src/ai_client/gemini_generative_model.dart
@@ -1,6 +1,7 @@
 // Copyright 2025 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 import 'package:firebase_ai/firebase_ai.dart';
 
 /// An interface for a generative model, allowing for mock implementations.
