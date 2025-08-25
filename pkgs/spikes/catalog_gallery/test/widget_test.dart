8acc7af858c8e1f993bbb60a98be2756b3479c6b
Polina Cherkasova <polina.c@live.com>
2025-08-25 08:55:34 -0700

    -

--- pkgs/spikes/catalog_gallery/test/widget_test.dart
+++ pkgs/spikes/catalog_gallery/test/widget_test.dart
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:catalog_gallery/main.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
