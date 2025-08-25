44152efd92324e400222852d3013b69bd8d5e0fb
Polina Cherkasova <polina.c@live.com>
2025-08-25 06:55:17 -0700

    -

--- pkgs/spikes/catalog_gallery/test/widget_test.dart
+++ pkgs/spikes/catalog_gallery/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:catalog_gallery/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CatalogGalleryApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
