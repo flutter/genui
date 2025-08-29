a871abee5fe0e6b59b0e419c3c85b9552aedfae4
Polina Cherkasova <polina.c@live.com>
2025-08-29 12:19:45 -0700

    Update examples/travel_app/lib/main.dart
    Co-authored-by: Greg Spencer <gspencergoog@users.noreply.github.com>

--- examples/travel_app/lib/main.dart
+++ examples/travel_app/lib/main.dart
@@ -405,7 +405,7 @@ following asset images:
 
 ${_imagesJson ?? ''}
 
-If there is no needed image in assets, use text description instead of images.
+If you can't find an appropriate image in the assets, use a text description instead.
 
 
 Here is an example of the arguments to the `addOrUpdateSurface` tool. Note that
