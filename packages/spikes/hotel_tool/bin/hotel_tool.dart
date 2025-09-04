7dac046e9a60cbe456928bb00bec3dfda565cb67
Polina Cherkasova <polina.c@live.com>
2025-09-04 13:32:28 -0700

    -

--- packages/spikes/hotel_tool/bin/hotel_tool.dart
+++ packages/spikes/hotel_tool/bin/hotel_tool.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+
 import 'package:hotel_tool/hotel_tool.dart';
 import 'api_key.dart';
 
 Future<void> main(List<String> arguments) async {
   final connect = SerpConnect(apiKey);
+
+  final data = await connect.fetchHotelData('Molokai, Hawaii');
+
+  print(formatJson(data));
+}
+
+String formatJson(Map<String, Object?> json) {
+  return const JsonEncoder.withIndent('  ').convert(json);
 }
