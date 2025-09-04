6bde9191db4f5a0e0efeac1cb7dd27f421661406
Polina Cherkasova <polina.c@live.com>
2025-09-04 12:35:43 -0700

    -

---
+++ packages/spikes/hotel_tool/bin/hotel_tool.dart
@@ -0,0 +1,5 @@
+import 'package:hotel_tool/hotel_tool.dart' as hotel_tool;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hotel_tool.calculate()}!');
+}
---
+++ packages/spikes/hotel_tool/test/hotel_tool_test.dart
@@ -0,0 +1,8 @@
+import 'package:hotel_tool/hotel_tool.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
