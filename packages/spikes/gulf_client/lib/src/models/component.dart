2994003c1103a2d554df3ce537855bc018fde6cc
Greg Spencer <gspencer@google.com>
2025-09-18 11:01:20 -0700

    feat(gulf-client): implement fromJson for new data models
    Implements the `fromJson` factory constructors for all new data model classes, enabling the client to parse a JSONL stream conforming to the new schema.
    
    This includes the deserialization logic for:
    - The `GulfStreamMessage` sealed class and its variants.
    - The `Component` class.
    - The `ComponentProperties` discriminated union and all of its specific subclasses.
    - All helper classes (e.g., `BoundValue`, `Children`, `Action`).

--- packages/spikes/gulf_client/lib/src/models/component.dart
+++ packages/spikes/gulf_client/lib/src/models/component.dart
@@ -9,6 +9,16 @@ class Component {
     required this.componentProperties,
   });
 
+  factory Component.fromJson(Map<String, dynamic> json) {
+    return Component(
+      id: json['id'] as String,
+      weight: (json['weight'] as num?)?.toDouble(),
+      componentProperties: ComponentProperties.fromJson(
+        json['componentProperties'] as Map<String, dynamic>,
+      ),
+    );
+  }
+
   final String id;
   final double? weight;
   final ComponentProperties componentProperties;
@@ -16,13 +26,60 @@ class Component {
 
 sealed class ComponentProperties {
   factory ComponentProperties.fromJson(Map<String, dynamic> json) {
-    // TODO(gspencer): implement fromJson
-    throw UnimplementedError();
+    final type = json.keys.first;
+    final properties = json[type] as Map<String, dynamic>;
+    switch (type) {
+      case 'Heading':
+        return HeadingProperties.fromJson(properties);
+      case 'Text':
+        return TextProperties.fromJson(properties);
+      case 'Image':
+        return ImageProperties.fromJson(properties);
+      case 'Video':
+        return VideoProperties.fromJson(properties);
+      case 'AudioPlayer':
+        return AudioPlayerProperties.fromJson(properties);
+      case 'Row':
+        return RowProperties.fromJson(properties);
+      case 'Column':
+        return ColumnProperties.fromJson(properties);
+      case 'List':
+        return ListProperties.fromJson(properties);
+      case 'Card':
+        return CardProperties.fromJson(properties);
+      case 'Tabs':
+        return TabsProperties.fromJson(properties);
+      case 'Divider':
+        return DividerProperties.fromJson(properties);
+      case 'Modal':
+        return ModalProperties.fromJson(properties);
+      case 'Button':
+        return ButtonProperties.fromJson(properties);
+      case 'CheckBox':
+        return CheckBoxProperties.fromJson(properties);
+      case 'TextField':
+        return TextFieldProperties.fromJson(properties);
+      case 'DateTimeInput':
+        return DateTimeInputProperties.fromJson(properties);
+      case 'MultipleChoice':
+        return MultipleChoiceProperties.fromJson(properties);
+      case 'Slider':
+        return SliderProperties.fromJson(properties);
+      default:
+        throw Exception('Unknown component type: $type');
+    }
   }
 }
 
 class HeadingProperties implements ComponentProperties {
   const HeadingProperties({required this.text, required this.level});
+
+  factory HeadingProperties.fromJson(Map<String, dynamic> json) {
+    return HeadingProperties(
+      text: BoundValue.fromJson(json['text'] as Map<String, dynamic>),
+      level: json['level'] as String,
+    );
+  }
 
   final BoundValue text;
   final String level;
@@ -31,11 +88,23 @@ class HeadingProperties implements ComponentProperties {
 class TextProperties implements ComponentProperties {
   const TextProperties({required this.text});
 
+  factory TextProperties.fromJson(Map<String, dynamic> json) {
+    return TextProperties(
+      text: BoundValue.fromJson(json['text'] as Map<String, dynamic>),
+    );
+  }
+
   final BoundValue text;
 }
 
 class ImageProperties implements ComponentProperties {
   const ImageProperties({required this.url});
+
+  factory ImageProperties.fromJson(Map<String, dynamic> json) {
+    return ImageProperties(
+      url: BoundValue.fromJson(json['url'] as Map<String, dynamic>),
+    );
+  }
 
   final BoundValue url;
 }
@@ -43,11 +112,26 @@ class ImageProperties implements ComponentProperties {
 class VideoProperties implements ComponentProperties {
   const VideoProperties({required this.url});
 
+  factory VideoProperties.fromJson(Map<String, dynamic> json) {
+    return VideoProperties(
+      url: BoundValue.fromJson(json['url'] as Map<String, dynamic>),
+    );
+  }
+
   final BoundValue url;
 }
 
 class AudioPlayerProperties implements ComponentProperties {
   const AudioPlayerProperties({required this.url, this.description});
+
+  factory AudioPlayerProperties.fromJson(Map<String, dynamic> json) {
+    return AudioPlayerProperties(
+      url: BoundValue.fromJson(json['url'] as Map<String, dynamic>),
+      description: json['description'] != null
+          ? BoundValue.fromJson(json['description'] as Map<String, dynamic>)
+          : null,
+    );
+  }
 
   final BoundValue url;
   final BoundValue? description;
@@ -59,6 +143,14 @@ class RowProperties implements ComponentProperties {
     this.distribution,
     this.alignment,
   });
+
+  factory RowProperties.fromJson(Map<String, dynamic> json) {
+    return RowProperties(
+      children: Children.fromJson(json['children'] as Map<String, dynamic>),
+      distribution: json['distribution'] as String?,
+      alignment: json['alignment'] as String?,
+    );
+  }
 
   final Children children;
   final String? distribution;
@@ -72,6 +164,14 @@ class ColumnProperties implements ComponentProperties {
     this.alignment,
   });
 
+  factory ColumnProperties.fromJson(Map<String, dynamic> json) {
+    return ColumnProperties(
+      children: Children.fromJson(json['children'] as Map<String, dynamic>),
+      distribution: json['distribution'] as String?,
+      alignment: json['alignment'] as String?,
+    );
+  }
+
   final Children children;
   final String? distribution;
   final String? alignment;
@@ -84,6 +184,14 @@ class ListProperties implements ComponentProperties {
     this.alignment,
   });
 
+  factory ListProperties.fromJson(Map<String, dynamic> json) {
+    return ListProperties(
+      children: Children.fromJson(json['children'] as Map<String, dynamic>),
+      direction: json['direction'] as String?,
+      alignment: json['alignment'] as String?,
+    );
+  }
+
   final Children children;
   final String? direction;
   final String? alignment;
@@ -92,17 +200,37 @@ class ListProperties implements ComponentProperties {
 class CardProperties implements ComponentProperties {
   const CardProperties({required this.child});
 
+  factory CardProperties.fromJson(Map<String, dynamic> json) {
+    return CardProperties(child: json['child'] as String);
+  }
+
   final String child;
 }
 
 class TabsProperties implements ComponentProperties {
   const TabsProperties({required this.tabItems});
 
+  factory TabsProperties.fromJson(Map<String, dynamic> json) {
+    return TabsProperties(
+      tabItems: (json['tabItems'] as List<dynamic>)
+          .map((e) => TabItem.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
+
   final List<TabItem> tabItems;
 }
 
 class DividerProperties implements ComponentProperties {
   const DividerProperties({this.axis, this.color, this.thickness});
+
+  factory DividerProperties.fromJson(Map<String, dynamic> json) {
+    return DividerProperties(
+      axis: json['axis'] as String?,
+      color: json['color'] as String?,
+      thickness: (json['thickness'] as num?)?.toDouble(),
+    );
+  }
 
   final String? axis;
   final String? color;
@@ -115,6 +243,13 @@ class ModalProperties implements ComponentProperties {
     required this.contentChild,
   });
 
+  factory ModalProperties.fromJson(Map<String, dynamic> json) {
+    return ModalProperties(
+      entryPointChild: json['entryPointChild'] as String,
+      contentChild: json['contentChild'] as String,
+    );
+  }
+
   final String entryPointChild;
   final String contentChild;
 }
@@ -122,12 +257,26 @@ class ModalProperties implements ComponentProperties {
 class ButtonProperties implements ComponentProperties {
   const ButtonProperties({required this.label, required this.action});
 
+  factory ButtonProperties.fromJson(Map<String, dynamic> json) {
+    return ButtonProperties(
+      label: BoundValue.fromJson(json['label'] as Map<String, dynamic>),
+      action: Action.fromJson(json['action'] as Map<String, dynamic>),
+    );
+  }
+
   final BoundValue label;
   final Action action;
 }
 
 class CheckBoxProperties implements ComponentProperties {
   const CheckBoxProperties({required this.label, required this.value});
+
+  factory CheckBoxProperties.fromJson(Map<String, dynamic> json) {
+    return CheckBoxProperties(
+      label: BoundValue.fromJson(json['label'] as Map<String, dynamic>),
+      value: BoundValue.fromJson(json['value'] as Map<String, dynamic>),
+    );
+  }
 
   final BoundValue label;
   final BoundValue value;
@@ -140,6 +289,17 @@ class TextFieldProperties implements ComponentProperties {
     this.type,
     this.validationRegexp,
   });
+
+  factory TextFieldProperties.fromJson(Map<String, dynamic> json) {
+    return TextFieldProperties(
+      text: json['text'] != null
+          ? BoundValue.fromJson(json['text'] as Map<String, dynamic>)
+          : null,
+      label: BoundValue.fromJson(json['label'] as Map<String, dynamic>),
+      type: json['type'] as String?,
+      validationRegexp: json['validationRegexp'] as String?,
+    );
+  }
 
   final BoundValue? text;
   final BoundValue label;
@@ -155,6 +315,15 @@ class DateTimeInputProperties implements ComponentProperties {
     this.outputFormat,
   });
 
+  factory DateTimeInputProperties.fromJson(Map<String, dynamic> json) {
+    return DateTimeInputProperties(
+      value: BoundValue.fromJson(json['value'] as Map<String, dynamic>),
+      enableDate: json['enableDate'] as bool?,
+      enableTime: json['enableTime'] as bool?,
+      outputFormat: json['outputFormat'] as String?,
+    );
+  }
+
   final BoundValue value;
   final bool? enableDate;
   final bool? enableTime;
@@ -168,6 +337,18 @@ class MultipleChoiceProperties implements ComponentProperties {
     this.maxAllowedSelections,
   });
 
+  factory MultipleChoiceProperties.fromJson(Map<String, dynamic> json) {
+    return MultipleChoiceProperties(
+      selections: BoundValue.fromJson(
+        json['selections'] as Map<String, dynamic>,
+      ),
+      options: (json['options'] as List<dynamic>?)
+          ?.map((e) => Option.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      maxAllowedSelections: json['maxAllowedSelections'] as int?,
+    );
+  }
+
   final BoundValue selections;
   final List<Option>? options;
   final int? maxAllowedSelections;
@@ -175,6 +356,14 @@ class MultipleChoiceProperties implements ComponentProperties {
 
 class SliderProperties implements ComponentProperties {
   const SliderProperties({required this.value, this.minValue, this.maxValue});
+
+  factory SliderProperties.fromJson(Map<String, dynamic> json) {
+    return SliderProperties(
+      value: BoundValue.fromJson(json['value'] as Map<String, dynamic>),
+      minValue: (json['minValue'] as num?)?.toDouble(),
+      maxValue: (json['maxValue'] as num?)?.toDouble(),
+    );
+  }
 
   final BoundValue value;
   final double? minValue;
@@ -189,6 +378,15 @@ class BoundValue {
     this.literalBoolean,
   });
 
+  factory BoundValue.fromJson(Map<String, dynamic> json) {
+    return BoundValue(
+      path: json['path'] as String?,
+      literalString: json['literalString'] as String?,
+      literalNumber: (json['literalNumber'] as num?)?.toDouble(),
+      literalBoolean: json['literalBoolean'] as bool?,
+    );
+  }
+
   final String? path;
   final String? literalString;
   final double? literalNumber;
@@ -198,12 +396,30 @@ class BoundValue {
 class Children {
   const Children({this.explicitList, this.template});
 
+  factory Children.fromJson(Map<String, dynamic> json) {
+    return Children(
+      explicitList: (json['explicitList'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      template: json['template'] != null
+          ? Template.fromJson(json['template'] as Map<String, dynamic>)
+          : null,
+    );
+  }
+
   final List<String>? explicitList;
   final Template? template;
 }
 
 class Template {
   const Template({required this.componentId, required this.dataBinding});
+
+  factory Template.fromJson(Map<String, dynamic> json) {
+    return Template(
+      componentId: json['componentId'] as String,
+      dataBinding: json['dataBinding'] as String,
+    );
+  }
 
   final String componentId;
   final String dataBinding;
@@ -212,12 +428,28 @@ class Template {
 class TabItem {
   const TabItem({required this.title, required this.child});
 
+  factory TabItem.fromJson(Map<String, dynamic> json) {
+    return TabItem(
+      title: BoundValue.fromJson(json['title'] as Map<String, dynamic>),
+      child: json['child'] as String,
+    );
+  }
+
   final BoundValue title;
   final String child;
 }
 
 class Action {
   const Action({required this.action, this.context});
+
+  factory Action.fromJson(Map<String, dynamic> json) {
+    return Action(
+      action: json['action'] as String,
+      context: (json['context'] as List<dynamic>?)
+          ?.map((e) => ContextItem.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
 
   final String action;
   final List<ContextItem>? context;
@@ -226,12 +458,26 @@ class Action {
 class ContextItem {
   const ContextItem({required this.key, required this.value});
 
+  factory ContextItem.fromJson(Map<String, dynamic> json) {
+    return ContextItem(
+      key: json['key'] as String,
+      value: BoundValue.fromJson(json['value'] as Map<String, dynamic>),
+    );
+  }
+
   final String key;
   final BoundValue value;
 }
 
 class Option {
   const Option({required this.label, required this.value});
+
+  factory Option.fromJson(Map<String, dynamic> json) {
+    return Option(
+      label: BoundValue.fromJson(json['label'] as Map<String, dynamic>),
+      value: json['value'] as String,
+    );
+  }
 
   final BoundValue label;
   final String value;
--- packages/spikes/gulf_client/lib/src/models/stream_message.dart
+++ packages/spikes/gulf_client/lib/src/models/stream_message.dart
@@ -6,19 +6,49 @@ import 'component.dart';
 
 sealed class GulfStreamMessage {
   factory GulfStreamMessage.fromJson(Map<String, dynamic> json) {
-    // TODO(gspencer): implement fromJson
-    throw UnimplementedError();
+    if (json.containsKey('streamHeader')) {
+      return StreamHeader.fromJson(
+        json['streamHeader'] as Map<String, dynamic>,
+      );
+    }
+    if (json.containsKey('beginRendering')) {
+      return BeginRendering.fromJson(
+        json['beginRendering'] as Map<String, dynamic>,
+      );
+    }
+    if (json.containsKey('componentUpdate')) {
+      return ComponentUpdate.fromJson(
+        json['componentUpdate'] as Map<String, dynamic>,
+      );
+    }
+    if (json.containsKey('dataModelUpdate')) {
+      return DataModelUpdate.fromJson(
+        json['dataModelUpdate'] as Map<String, dynamic>,
+      );
+    }
+    throw Exception('Unknown message type in JSON: $json');
   }
 }
 
 class StreamHeader implements GulfStreamMessage {
   const StreamHeader({required this.version});
 
+  factory StreamHeader.fromJson(Map<String, dynamic> json) {
+    return StreamHeader(version: json['version'] as String);
+  }
+
   final String version;
 }
 
 class BeginRendering implements GulfStreamMessage {
   const BeginRendering({required this.root, this.styles});
+
+  factory BeginRendering.fromJson(Map<String, dynamic> json) {
+    return BeginRendering(
+      root: json['root'] as String,
+      styles: json['styles'] as Map<String, dynamic>?,
+    );
+  }
 
   final String root;
   final Map<String, dynamic>? styles;
@@ -27,11 +57,26 @@ class BeginRendering implements GulfStreamMessage {
 class ComponentUpdate implements GulfStreamMessage {
   const ComponentUpdate({required this.components});
 
+  factory ComponentUpdate.fromJson(Map<String, dynamic> json) {
+    return ComponentUpdate(
+      components: (json['components'] as List<dynamic>)
+          .map((e) => Component.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
+
   final List<Component> components;
 }
 
 class DataModelUpdate implements GulfStreamMessage {
   const DataModelUpdate({this.path, required this.contents});
+
+  factory DataModelUpdate.fromJson(Map<String, dynamic> json) {
+    return DataModelUpdate(
+      path: json['path'] as String?,
+      contents: json['contents'],
+    );
+  }
 
   final String? path;
   final dynamic contents;
