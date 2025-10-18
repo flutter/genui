91b0adf88f9cce07b384668c4d46236cae01c7a7
Polina Cherkasova <polina.c@live.com>
2025-10-18 11:20:31 -0700

    -

--- examples/catalog_gallery/test/widget_test.dart
+++ examples/catalog_gallery/test/widget_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Build the app and trigger a frame.
     await tester.pumpWidget(const CatalogGalleryApp());
   });
 }
