15204012b2cc3662824d014c49f17e404945cc8b
Polina Cherkasova <polina.c@live.com>
2025-10-17 22:56:48 -0700

    Update backend_api_test.dart

--- packages/spikes/custom_backend/test/backend_api_test.dart
+++ packages/spikes/custom_backend/test/backend_api_test.dart
@@ -8,6 +8,11 @@ import 'package:flutter_genui/flutter_genui.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 
+/*
+Prompt for this code:
+Update api.dart to make 'sendRequest works' passing.
+*/
+
 void main() {
   test('sendRequest works', () async {
     final protocol = Protocol();
