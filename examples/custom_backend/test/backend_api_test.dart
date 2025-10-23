93cbcb9f6e9abb9a1b0a13ccec8e73f4b481859b
Polina Cherkasova <polina.c@live.com>
2025-10-22 19:00:23 -0700

    -

--- examples/custom_backend/test/backend_api_test.dart
+++ examples/custom_backend/test/backend_api_test.dart
@@ -14,10 +14,11 @@ void main() {
   });
 
   for (final savedResponse in savedResponseAssets) {
-    // TODO: fix Gemini API keys to get live test working.
-    if (savedResponse == null) {
-      continue;
-    }
+    // // TODO: fix Gemini API keys to get live test working.
+    // if (savedResponse == null) {
+    //   continue;
+    // }
+
     // To update the saved responses, run the app, select "Request Gemini",
     // and copy the console output of the "Response body" to the
     // corresponding `saved-response-X.json` file in `assets/data/`.
@@ -30,10 +31,9 @@ void main() {
           savedResponse: savedResponse,
         );
         expect(result, isNotNull);
-        expect(result, isA<List<A2uiMessage>>());
-        expect(result!.length, 2);
-        expect(result[0], isA<SurfaceUpdate>());
-        expect(result[1], isA<BeginRendering>());
+        expect(result!.messages.length, 2);
+        expect(result.messages[0], isA<SurfaceUpdate>());
+        expect(result.messages[1], isA<BeginRendering>());
       },
       retry: 3,
       timeout: const Timeout(Duration(minutes: 2)),
