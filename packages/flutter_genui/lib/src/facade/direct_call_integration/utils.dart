e507e8191127358958c2c98778f76a3241dc0b5b
Polina Cherkasova <polina.c@live.com>
2025-10-22 19:27:12 -0700

    -

--- packages/flutter_genui/lib/src/facade/direct_call_integration/utils.dart
+++ packages/flutter_genui/lib/src/facade/direct_call_integration/utils.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import '../../model/a2ui_message.dart';
 import '../../model/a2ui_schemas.dart';
 import '../../model/catalog.dart';
