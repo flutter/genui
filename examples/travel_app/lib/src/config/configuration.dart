88ecd66115307e554035bc1212d9aea87537821e
Greg Spencer <gspencer@google.com>
2025-11-18 15:41:35 -0800

    Add copyrights

--- examples/travel_app/lib/src/config/configuration.dart
+++ examples/travel_app/lib/src/config/configuration.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 /// Enum for selecting which AI backend to use.
 enum AiBackend {
   /// Use Firebase AI
