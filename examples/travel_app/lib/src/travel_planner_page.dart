f759b412eff1ab710ac242cda784802cba97ed81
Polina Cherkasova <polina.c@live.com>
2025-12-08 17:12:51 -0800

    -

--- examples/travel_app/lib/src/travel_planner_page.dart
+++ examples/travel_app/lib/src/travel_planner_page.dart
@@ -63,16 +63,7 @@ class _TravelPlannerPageState extends State<TravelPlannerPage>
   @override
   void initState() {
     super.initState();
-    final genUiManager = GenUiManager(
-      catalog: travelAppCatalog,
-      configuration: const GenUiConfiguration(
-        actions: ActionsConfig(
-          allowCreate: true,
-          allowUpdate: true,
-          allowDelete: true,
-        ),
-      ),
-    );
+    final genUiManager = GenUiManager(catalog: travelAppCatalog);
     _userMessageSubscription = genUiManager.onSubmit.listen(
       _handleUserMessageFromUi,
     );
