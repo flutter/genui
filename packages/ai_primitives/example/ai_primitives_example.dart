527163bbb7320e911c265d0bab0d6578592448ca
Polina Cherkasova <polina.c@live.com>
2025-12-16 11:41:09 -0800

    -

--- packages/ai_primitives/example/ai_primitives_example.dart
+++ packages/ai_primitives/example/ai_primitives_example.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:ai_primitives/ai_primitives.dart';
 
 void main() {
