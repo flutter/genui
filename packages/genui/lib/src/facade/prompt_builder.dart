b80b8b49fe8c3d88b8315a0024c233f421bbe9fd
Polina Cherkasova <polina.c@live.com>
2026-02-12 21:12:45 -0800

    -

--- packages/genui/lib/src/facade/prompt_builder.dart
+++ packages/genui/lib/src/facade/prompt_builder.dart
@@ -1,20 +1,53 @@
 import '../../genui.dart';
 
-class SurfaceOperations {
-  const SurfaceOperations({
-    this.create = true,
-    this.update = false,
-    this.delete = false,
-  });
-
-  final bool create;
-  final bool update;
-  final bool delete;
-}
+// TODO(polina-c): add allowed surface operations
+// TODO(polina-c): consider incorporating catalog rules to the catalog
 
 class PromptBuilder {
-  PromptBuilder(this.catalog, this.allowedOperations);
+  PromptBuilder({required this.catalog, this.instructions});
 
+  /// Instructions for the generated UI.
+  ///
+  /// This can include description of target user profile,
+  /// description of the typical tasks the user wants to perform,
+  /// wanted profile of the AI agent, examples of good responses,
+  /// explanation when to use which catalog items.
+  final String? instructions;
+
+  /// Catalog to use for the generated UI.
   final Catalog catalog;
-  final SurfaceOperations allowedOperations;
+
+  late final String prompt = () {
+    final String a2uiSchema = A2uiMessage.a2uiMessageSchema(
+      catalog,
+    ).toJson(indent: '  ');
+
+    return '''
+$instructions
+
+IMPORTANT: When you generate UI in a response, you MUST always create
+a new surface with a unique `surfaceId`. Do NOT reuse or update
+existing `surfaceId`s. Each UI response must be in its own new surface.
+
+<a2ui_schema>
+$a2uiSchema
+</a2ui_schema>
+
+${StandardCatalogEmbed.standardCatalogRules}
+
+$_basicChatPromptFragment''';
+  }();
 }
+
+/// A basic chat prompt fragment.
+const String _basicChatPromptFragment = '''
+
+# Outputting UI information
+
+Use the provided tools to respond to the user using rich UI elements.
+
+Important considerations:
+- When you are asking for information from the user, you should always include
+  at least one submit button of some kind or another submitting element so that
+  the user can indicate that they are done providing information.
+''';
