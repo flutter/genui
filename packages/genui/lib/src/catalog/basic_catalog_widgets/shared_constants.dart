ebace5d983cbd228e9c1b2fe16b7448335010bad
Polina Cherkasova <polina.c@live.com>
2026-02-17 10:14:58 -0800

    Update shared_constants.dart

--- packages/genui/lib/src/catalog/basic_catalog_widgets/shared_constants.dart
+++ packages/genui/lib/src/catalog/basic_catalog_widgets/shared_constants.dart
@@ -1,4 +1,4 @@
 class SharedConstants {
-  /// The default spacing where bigger some non-zero spacing is needed.
+  /// The default spacing where some non-zero spacing is needed.
   static const double spacing = 8;
 }
