26e3305df99a34c8eacf4da96c6f4df545e0a0fb
Andrew Kolos <andrewrkolos@gmail.com>
2026-02-19 13:27:01 -0800

    init

---
+++ examples/composer/lib/api_key/web_get_api_key.dart
@@ -0,0 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+String getApiKey() {
+  throw UnsupportedError('Web API key retrieval is not supported.');
+}
