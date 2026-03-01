e4b0b665ab769b1ee3fa5290eeb6a8119a5e065b
Polina Cherkasova <polina.c@live.com>
2026-03-01 12:59:35 -0800

    -

--- packages/genui/test/test_infra/golden_texts.dart
+++ packages/genui/test/test_infra/golden_texts.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
 /// Verifies that the given [text] matches the content of the golden file.
 ///
 /// If [autoUpdateGoldenFiles] is true, the golden file will be updated.
 void verifyGoldenText(String text, String goldenFileName) {
-  if (autoUpdateGoldenFiles) {}
+  final String goldenFilePath = _goldenFilePath(goldenFileName);
+
+  if (autoUpdateGoldenFiles) {
+    File(goldenFilePath).writeAsStringSync(text);
+  } else {
+    final String goldenFileContent = File(goldenFilePath).readAsStringSync();
+    expect(goldenFileContent, text);
+  }
 }
 
 /// Returns absolute path to the golden file.
@@ -13,4 +22,12 @@ void verifyGoldenText(String text, String goldenFileName) {
 ///
 /// For example, if the test file is `utils/my_main_test.dart`,
 /// the golden file will be in `utils/my_main_test.golden/[file_name]`.
-String _goldenFilePath(String fileName) {}
+String _goldenFilePath(String fileName) {
+  final Uri scriptUri = Platform.script;
+  final String scriptName = scriptUri.pathSegments.last;
+  final String stem = scriptName.endsWith('.dart')
+      ? scriptName.substring(0, scriptName.length - 5)
+      : scriptName;
+  final Uri goldenDir = scriptUri.resolve('$stem.golden/');
+  return goldenDir.resolve(fileName).toFilePath();
+}
