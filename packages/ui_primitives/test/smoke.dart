83535e11961a2f915e69821aad3dd94ba0feff0c
Polina Cherkasova <polina.c@live.com>
2026-03-24 15:53:22 -0700

    Update smoke.dart

--- packages/ui_primitives/test/smoke.dart
+++ packages/ui_primitives/test/smoke.dart
@@ -1,5 +1,18 @@
 import 'package:test/test.dart';
+import 'package:ui_primitives/ui_primitives.dart';
 
 void main() {
-  test('smoke', () {});
+  test('smoke', () {
+    final ValueNotifier<int> notifier = ValueNotifier(1);
+    addTearDown(notifier.dispose);
+    var count = 0;
+    notifier.addListener(() => count++);
+
+    expect(notifier.value, 1);
+    expect(count, 0);
+
+    notifier.value = 2;
+    expect(notifier.value, 2);
+    expect(count, 1);
+  });
 }
