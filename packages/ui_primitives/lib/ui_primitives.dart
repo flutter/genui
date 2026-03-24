c76611bd0449dbf8bf41852235fa2dbac33d52a8
Polina Cherkasova <polina.c@live.com>
2026-03-24 16:53:48 -0700

    Update ui_primitives.dart

--- packages/ui_primitives/lib/ui_primitives.dart
+++ packages/ui_primitives/lib/ui_primitives.dart
@@ -2,8 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export 'src/foundation/assertions.dart';
 export 'src/foundation/listenable.dart';
-export 'src/foundation/print.dart';
-export 'src/foundation/stack_frame.dart';
 export 'src/foundation/value_notifier.dart';
