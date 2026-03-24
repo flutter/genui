42bcb79b28dacebfa575bc3f07f842ead301f214
Polina Cherkasova <polina.c@live.com>
2026-03-24 16:01:58 -0700

    -

--- packages/ui_primitives/lib/src/primitives/private_leak_tracking.dart
+++ packages/ui_primitives/lib/src/primitives/private_leak_tracking.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:leak_tracker/leak_tracker.dart';
 
 // TODO(polinach): move this constant to leak_tracker package.
