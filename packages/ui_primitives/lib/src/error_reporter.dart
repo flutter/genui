63ae809739ca8254f1a15f881271465278caf4f9
Polina Cherkasova <polina.c@live.com>
2026-03-28 16:59:48 -0700

    Update error_reporter.dart

--- packages/ui_primitives/lib/src/error_reporter.dart
+++ packages/ui_primitives/lib/src/error_reporter.dart
@@ -1,13 +1,14 @@
 import 'foundation/assertions.dart';
 
 class FrameworkErrorReporter {
+  /// Set this instance to customize error reporting for the framework.
   static FrameworkErrorReporter instance = FrameworkErrorReporter();
 
   /// Creates a new [FrameworkError] instance.
   FrameworkError create({String? message, FrameworkErrorDetails? details}) =>
       FrameworkError(message: message, details: details);
 
-  /// Reports a [FrameworkError] according to framework settings.
+  /// Reports a [FrameworkError] according to the framework settings.
   void report({String? message, FrameworkErrorDetails? details}) =>
       throw FrameworkError(message: message, details: details);
 }
