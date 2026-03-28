a9c8a05f69aa32a038480bbcaef69f2b84bd029a
Polina Cherkasova <polina.c@live.com>
2026-03-28 16:31:11 -0700

    Delete error_reporter.dart

--- packages/ui_primitives/lib/src/error_reporter.dart
+++
@@ -1,9 +0,0 @@
-import 'foundation/assertions.dart';
-
-class FrameworkErrorReporter {
-  static FrameworkErrorReporter instance = FrameworkErrorReporter();
-
-  Error create(FrameworkErrorDetails details) => Error();
-
-  void report(FrameworkErrorDetails details) => throw create(details);
-}
