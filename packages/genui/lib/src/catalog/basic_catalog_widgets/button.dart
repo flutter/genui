4f8df0555d3a30c48b444bf043f5303a77390425
Jia Hao Goh <jiahaog@users.noreply.github.com>
2026-03-30 02:56:09 +0000

    Fix imports

--- packages/genui/lib/src/catalog/basic_catalog_widgets/button.dart
+++ packages/genui/lib/src/catalog/basic_catalog_widgets/button.dart
@@ -8,9 +8,9 @@ import 'package:json_schema_builder/json_schema_builder.dart';
 import '../../model/a2ui_schemas.dart';
 import '../../model/catalog_item.dart';
 import '../../model/ui_models.dart';
+import '../../model/validation_helper.dart';
 import '../../primitives/logging.dart';
 import '../../primitives/simple_items.dart';
-import '../../model/validation_helper.dart';
 import '../../widgets/widget_utilities.dart';
 
 final _schema = S.object(
