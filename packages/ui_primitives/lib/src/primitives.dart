684e4e45106e5fde0c22176150db73e8f3491662
Polina Cherkasova <polina.c@live.com>
2026-03-30 09:21:49 -0700

    Update primitives.dart

--- packages/ui_primitives/lib/src/primitives.dart
+++ packages/ui_primitives/lib/src/primitives.dart
@@ -1,5 +1,3 @@
-const String libraryName = 'ui_primitives';
-
 // Copyright 2025 The Flutter Authors.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
