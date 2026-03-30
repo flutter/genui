0508c1a7fe5e6b47f35a3c00216db4ba8c3a97c8
Polina Cherkasova <polina.c@live.com>
2026-03-30 10:26:42 -0700

    Update error_reporter.dart

--- packages/ui_primitives/lib/src/error_reporter.dart
+++ packages/ui_primitives/lib/src/error_reporter.dart
@@ -6,18 +6,18 @@ class FrameworkErrorReporter {
   /// Set this instance to customize error reporting for the framework.
   static FrameworkErrorReporter instance = FrameworkErrorReporter();
 
-  Error error(String message) => FrameworkError(message: message);
+  Error error(String message) => _FrameworkError(message: message);
 
   /// Reports [FrameworkErrorDetails] according to the framework settings.
   ///
   /// Depending on settings, it may throw an exception, log an error,
   /// or debug-stop the execution.
   void report(FrameworkErrorDetails details) =>
-      throw FrameworkError(details: details);
+      throw _FrameworkError(details: details);
 }
 
-final class FrameworkError extends Error {
-  FrameworkError({this.message, this.details});
+final class _FrameworkError extends Error {
+  _FrameworkError({this.message, this.details});
 
   final FrameworkErrorDetails? details;
   final String? message;
