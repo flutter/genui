d0ee3b1cbc22a27cbd20c94c80654369bb26b571
Polina Cherkasova <polina.c@live.com>
2026-05-09 14:49:09 -0700

    reorg

---
+++ examples/simple_chat/lib/primitives/simple_items.dart
@@ -0,0 +1,11 @@
+/// Defines the mode of the application.
+enum AppMode {
+  /// Agent responds with text only.
+  textOnly,
+
+  /// Agent responds with text and the basic catalog items.
+  basicCatalog,
+
+  /// Agent responds with text and custom catalog items.
+  customCatalog,
+}
