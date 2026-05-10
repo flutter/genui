53a75db967a691041439dc315affa976eae51cd2
Polina Cherkasova <polina.c@live.com>
2026-05-10 15:48:08 -0700

    -

--- examples/yg_demo/android/app/src/main/kotlin/com/example/cn26demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cn26demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- examples/yg_demo/lib/primitives/api_key/get_api_key.dart
+++
@@ -1,3 +0,0 @@
-import 'io_get_api_key.dart' if (dart.library.html) 'web_get_api_key.dart';
-
-String getApiKey() => platformGetApiKey();
