221751508698894dc87ac384fb7896d838364d72
Greg Spencer <gspencer@google.com>
2026-05-26 10:12:48 -0700

    Fix copyright

--- packages/genui_express/macos/genui_express/Package.swift
+++ packages/genui_express/macos/genui_express/Package.swift
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
