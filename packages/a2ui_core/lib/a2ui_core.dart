fe8b98cfc642deb083870081642fca1286f0f5d6
Polina Cherkasova <polina.c@live.com>
2026-08-10 17:52:34 -0700

    Update a2ui_core.dart

--- packages/a2ui_core/lib/a2ui_core.dart
+++ packages/a2ui_core/lib/a2ui_core.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// DO NOT MERGE
+
 /// Core A2UI protocol implementation for Dart.
 library;
 
